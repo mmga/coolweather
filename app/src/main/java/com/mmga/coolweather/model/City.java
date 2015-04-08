6690a34590734678b42b7e399197872048f05a6c
mmga <1034752946@qq.com>
2015-04-08 17:25:05 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/mmga/coolweather/model/City.java
@@ -0,0 +1,43 @@
+package com.mmga.coolweather.model;
+
+/**
+ * Created by mmga on 2015/4/8.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
